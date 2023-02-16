f7f6ecb9d1a2c4f370b6cc8c50dc0f7a3fe3587c
Elayaraman-Ramalingam <116268532+Elayaraman-Ramalingam@users.noreply.github.com>
2023-02-17 04:20:23 +0530

    finishing up javascript for user CRUD

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,6 +1,19 @@
+function findObj(x, y){
+    for (i=0;i< y.length;i++){
+        if(x === y[i]);
+        return y[i];
+    }
+}
 
-let profile = document.createElement("div");
-profile.setAttribute("id","form");
-profile.setAttribute("class","profile");
+function showDetails(){
 
+    const userId = JSON.parse(localStorage.getItem("currentUser"));
+    const userList = JSON.parse(localStorage.getItem("users"));
 
+    document.getElementById("username").value = userId["username"];
+    document.getElementById("tel").value = userId["tel"];
+
+    console.log(findObj(userId.username,userList));
+    }
+
+      
