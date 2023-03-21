fb77076dbae66161a9758f2f85bf37509bb95e94
Elayaraman-Ramalingam <116268532+Elayaraman-Ramalingam@users.noreply.github.com>
2023-03-21 16:07:46 +0530

    First commit of version 3-ui

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,58 +1,9 @@
+function appndmsg(e){
+    console.log("appndmsg");
+    e.preventDefault();
 
-console.log("hai");
-
-let message_list = JSON.parse(localStorage.getItem("message_list"));
+    let message_list = JSON.parse(localStorage.getItem("message_list"));
     if(!message_list){
         message_list = []
     }
-
-    console.log(message_list);
-
-    for (let msg_list of message_list){
-    let msg_card = document.createElement("div");
-    msg_card.setAttribute("class","chat_right");
-
-    let msg_content = document.createElement("div");
-    msg_content.setAttribute("class","right message");
-    msg_card.append(msg_content);
-
-    let msg_str = document.createElement("p");
-    msg_str.innerHTML = msg_list.message;
-    msg_content.append(msg_str);
-
-    let msg_time = document.createElement("span");
-    msg_time.innerHTML = msg_list.msg_time;
-    msg_content.append(msg_time);
-
-    document.querySelector("div.chat").append(msg_card);
 }
-
-let chat_bar = document.querySelector(".chat_bar");
-
-chat_bar.addEventListener("submit", function(m){
-
-    m.preventDefault;
-
-    const message_input = document.getElementById("message_input").value.trim();
-
-    if(message_input){
-
-    const message_time = new Date();
-    const hours = message_time.getHours() > 12 ? message_time.getHours() - 12 : message_time.getHours();
-    const minutes = message_time.getMinutes() < 10 ? '0' + message_time.getMinutes() : message_time.getMinutes();
-    const ampm = message_time.getHours() >= 12 ? 'PM' : 'AM';
-    const time = hours + ':' + minutes + ' ' + ampm;
-
-    const message = {
-        id: Date.now(),
-        message:message_input,
-        msg_time:time
-    };
-
-    message_list.push(message);
-
-    localStorage.setItem("message_list",JSON.stringify(message_list));
-        
-    }
-
-    });
---
+++ assets/js/profile.js
@@ -0,0 +1,50 @@
+let currentUser = JSON.parse(sessionStorage.getItem("tempuser"));
+
+if(!currentUser){
+    currentUser = JSON.parse(localStorage.getItem("currentUser"));
+}
+let UserList = JSON.parse(localStorage.getItem("userList"));
+
+let index = 0;
+for(let i = 0; i<UserList.length;i++){
+    if(currentUser.userph == UserList[i].userph){
+        index = i;
+        break;
+    }
+}
+
+const user = UserList[index];
+
+function profile(){
+    document.getElementById("username").value = user.username;
+    document.getElementById("userph").value = user.userph;
+}
+
+function logout(){
+    sessionStorage.clear();
+    localStorage.removeItem("currentUser");
+
+    window.location.href="../index.html";
+}
+
+function submit(){
+
+    let username = document.getElementById("username").value.trim();
+    currentUser.username = username;
+    UserList[index].username = username;
+
+    let passwd = prompt("Enter your password");
+    if(passwd === currentUser.passwd){
+        localStorage.setItem( "currentUser" ,JSON.stringify(currentUser));
+        sessionStorage.setItem("tempuser",JSON.stringify(currentUser));
+        localStorage.setItem( "userList" ,JSON.stringify(UserList));
+
+        window.location.href="profile.html";
+    }
+    else{
+        alert("Password is incorrect");
+        return;
+    }
+
+}
+
