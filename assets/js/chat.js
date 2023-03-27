f3af2fecdb2245ee0d7c454ec136d9742d8586f4
Elayaraman-Ramalingam <116268532+Elayaraman-Ramalingam@users.noreply.github.com>
2023-03-27 14:05:40 +0530

    feat: chat page main commit

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,3 +1,4 @@
+
 let UserList = JSON.parse(localStorage.getItem("userList"));
 let currentUser = JSON.parse(localStorage.getItem("currentUser"));
 let userid = currentUser.userph;
@@ -6,6 +7,14 @@ const params = new URLSearchParams(window.location.search);
 const id = params.get('id');
 const user = UserList.find(UserList => UserList.userph === id);
 
+const user1 = UserList.find(UserList => UserList.userph === userid);
+const user2 = UserList.find(UserList => UserList.userph === id);
+
+const convoid = parseInt(user1.userph) + parseInt(user2.userph);
+
+const convo_list =  JSON.parse(localStorage.getItem("convo_list")) || [];
+
+const convo = convo_list.find(convo => convo.id === convoid);
 
 const headerTemplate = `
     <header class="c-head navbar">
@@ -25,9 +34,8 @@ const headerTemplate = `
 `;
 
 function addHeader() {
-    const body = document.querySelector('body');
-    body.insertAdjacentHTML('afterbegin', headerTemplate);
-    }
+    document.querySelector('body').insertAdjacentHTML('afterbegin', headerTemplate);
+}
 
 // script for uuid
 const script = document.createElement('script');
@@ -35,40 +43,76 @@ script.src = 'https://cdn.jsdelivr.net/npm/uuid@9.0.0/dist/index.min.js';
 script.async = true;
 document.head.appendChild(script);
 
-
-let message_list = JSON.parse(localStorage.getItem("message_list"));
-if(!message_list){
-    message_list = []
-}
-
-function Chat(){
-
-    let UserList = JSON.parse(localStorage.getItem("userList"));
-    let currentUser = JSON.parse(localStorage.getItem("currentUser"));
-    let userid = currentUser.userph;
-
-    const user1 = UserList.find(UserList => UserList.userph === userid);
-    const user2 = UserList.find(UserList => UserList.userph === id);
-
-    const convoid = parseInt(user1.userph) + parseInt(user2.userph);
-
-    const convo = {
-        convoid: convoid,
-        user1: user1.userph,
-        user2: user2.userph
-    };
-
-    console.log(convo);
-    message_list.push(convo);
-
-    // localStorage.setItem('message_list', JSON.stringify(message_list));
-
-    console.log(user2);
-    console.log(user1);
+// for time stamp
+async function loadMessages() {
+  // code to load messages
+    const convo = convo_list.find(convo => convo.id === convoid);
+    if (convo && convo.messages && convo.messages.length > 0) {
+        const messages = convo.messages.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
+        const messageContainer = document.querySelector(".c-holder");
+        let html = "";
+        for (const message of messages) {
+        const senderClass = message.sender === user1.userph ? "out" : "in";
+        html += `
+            <div class="message ${senderClass}">
+            <div class="msg">
+                <p>${message.message}</p>
+            </div>
+            <div class="time">
+                <span>${message.time}</span>
+            </div>
+            </div>
+        `;
+        }
+        messageContainer.innerHTML = html;
     }
-
-function appndmsg(e){
-    console.log("appndmsg");
-    e.preventDefault();
-
 }
+
+async function Chat() {
+    let convo = convo_list.find(convo => convo.id === convoid);
+    if (!convo && user1.userph !== user2.userph) {
+        convo = {
+            id: convoid,
+            user1: user1.userph,
+            user2: user2.userph,
+            messages: []
+        };
+        convo_list.push(convo);
+        localStorage.setItem("convo_list", JSON.stringify(convo_list));
+    }
+    return convo;
+}
+
+
+async function appndmsg(e) {
+    e.preventDefault();
+    const convo = await Chat();
+    let message = document.getElementById("msg_val").value.trim();
+    if (message) {
+        if (!convo.messages) {
+            convo.messages = [];
+        }
+        convo.messages.push({
+            id: uuidv4(),
+            message: message,
+            sender: user1.userph,
+            receiver: user2.userph,
+            time: new Date().toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }),
+            timestamp : new Date
+        });
+        document.getElementById("msg_val").value = ""; 
+    }
+    localStorage.setItem("convo_list", JSON.stringify(convo_list));
+
+    loadMessages();
+}
+
+function handleStorageChange(event) {
+        if (event.key === "convo_list") {
+        loadMessages();
+        }
+    }
+    
+    window.addEventListener("storage", handleStorageChange);
+
+Chat();
