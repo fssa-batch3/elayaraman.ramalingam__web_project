588c003c330de062d3a7e5f98da4839e7d880c60
Elayaraman-Ramalingam <116268532+Elayaraman-Ramalingam@users.noreply.github.com>
2023-05-22 07:27:33 +0530

    fix: final commit 2

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -30,10 +30,10 @@ const headerTemplate = `
         <header class="c-head navbar navbar-fixed-top width">
             <div class="container-sm">
                 <div>
-                    <button class="btn" onclick="window.history.back()">
+                    <button class="btn" onclick="window.location.href='./home.html'">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-</svg>
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+                    </svg>
 
                     </button>
                 </div>
